818c00869d5f2bb67651f8205e4358fd61a97af2
josbor1 <josbor1@ilstu.edu>
2025-01-19 22:49:30 -0600

    Added IF statement

--- Blink/Blink.ino
+++ Blink/Blink.ino
@@ -26,6 +26,8 @@
 void setup() {
   // initialize digital pin LED_BUILTIN as an output.
   pinMode(LED_BUILTIN, OUTPUT);
+  if //stuff 
+  // do stuff
 }
 
 // the loop function runs over and over again forever
